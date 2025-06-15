e674f4a62b681bf6fed9a9e16e7de275420d4d6c
Cory Hudson <64428121+wind197@users.noreply.github.com>
2025-06-15 14:35:33 -0500

    Moved home to dedicated component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {Github} from './github/github';
+import {About} from './about/about';
+import {Home} from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Home
+  },
+  {
+    path: "github",
+    component: Github
+  },
+  {
+    path: "about",
+    component: About
+  }
+];
--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatButton,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
